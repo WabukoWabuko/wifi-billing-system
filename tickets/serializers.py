0bd583c8d65184df03fad41c9f7cd5c72f9a3d0d
WabukoWabuko <basilwabbs@gmail.com>
2025-04-15 17:35:37 +0300

    Create serializers for all models

---
+++ tickets/serializers.py
@@ -0,0 +1,9 @@
+# tickets/serializers.py
+
+from rest_framework import serializers
+from .models import Ticket
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all__'
---
+++ usage/serializers.py
@@ -0,0 +1,9 @@
+# usage/serializers.py
+
+from rest_framework import serializers
+from .models import Usage
+
+class UsageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usage
+        fields = '__all__'
